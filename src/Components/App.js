c26de8eb0e8829e5073e4903773d309e9031d73c
Amethyst Williams <zmithic@gmail.com>
2021-11-03 15:35:33 -0500

    Successfully imported axios and used useEffect to get data from BASE_URL with API_KEY

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,7 +11,22 @@ import Header from './Header';
 import LeftBox from './LeftBox';
 import Article from './Article';
 
+
 function App() {
+  // Setting state for NASA pics
+  const [nasaPic, setNasaPic] = useState(null);
+
+  // Calling API
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.error(`You fucked up! ${err}`);
+      })
+  }, []);
+
   return (
     <div className="App">
       <p>
@@ -19,12 +34,12 @@ function App() {
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
         <br />
         You are gae!
+        </p>
         <br />
         <h2>Here's one of the cutest Pokemon!</h2>
         <img src='https://projectpokemon.org/home/uploads/monthly_2017_11/59fbadf186d7a_large.GlobalLink.png.b152ebe5c9a45a81d85a2d85387931a4.png' alt='Shinx the cutie' width='500' />
         <br />
         <h3>It's <u>Shinx</u>!</h3>
-      </p>
     </div>
   );
 }
